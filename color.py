7fb63afab05690d5226da38bc69c6a95a09ddb2f
Dona m Thomas <95832329+rmca2023@users.noreply.github.com>
2022-03-09 22:33:12 +0530

    Add files via upload

---
+++ color.py
@@ -0,0 +1,9 @@
+set1 = ["red","green","blue"]
+set2 = ["white","red", "black"]
+print("List1 color is:",set1)
+print("List2 color is:",set2)
+set3 = set1 + set2
+#set3 = set1.difference(set2)
+print("The color is:", set3)
+
+
---
+++ time.py
@@ -0,0 +1,33 @@
+class Time:
+    def __init__(self,h,m,s):
+      self.__h = h1
+      self.__m = m1
+      self.__s = s1
+    def __int__(self,x):
+     sum1 = self.__h1+self.__x
+     sum2 = self.__m1+self.__x
+     sum3 = self.__s1+self.__x
+
+print("Time1:")
+h1=int(input("Enter the hour1:"))
+m1=int(input("Enter the min1:"))
+s1=int(input("Enter the sec1:"))
+
+
+print("Time2:")
+h2=int(input("Enter the hour2:"))
+m2=int(input("Enter the min2:"))
+s2=int(input("Enter the sec2:"))
+obj2=Time(h2,m2,s2)
+print("Sum of two time:")
+
+h=h1+h2
+print(h)
+m=m1+m2
+print(m)
+s=s1+s2
+print(s)
+
+
+
+
